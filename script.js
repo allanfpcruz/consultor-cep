bad9144ca2d64feb3c7eed06cf08b379ea05b701
allanfpcruz <137228336+allanfpcruz@users.noreply.github.com>
2023-11-08 16:18:21 -0300

    projeto consultor de cep

---
+++ script.js
@@ -0,0 +1,33 @@
+//viacep.com.br/ws/01001000/json/
+
+const cep = document.querySelector("#cep")
+
+const showData = (results) => {
+    for(const campo in results) {
+        if (document.querySelector('#' + campo)) {
+            document.querySelector('#' + campo).value = results[campo]
+        }
+    }
+}
+cep.addEventListener("blur",(e) => {
+    let newCep = cep.value.replace("-", "")
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'default'
+    }
+    fetch(`https://viacep.com.br/ws/${newCep}/json/`, options)
+        .then(response => {response.json()
+            .then(data => {
+                console.log(data)  
+                showData(data) 
+                if (data.erro) {
+                    document.querySelector('#erro').classList.remove('hide')
+                    removeData(data)
+                } else {
+                    document.querySelector('#erro').classList.add('hide')
+                    
+                }
+            })
+        })
+})
